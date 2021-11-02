4998f8111de8b6bb31ab9ac35790263983588308
Julien Genestoux <julien.genestoux@gmail.com>
2021-11-02 15:10:11 -0400

    adding lock to premium section

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const zones = require('./zones')
-
+const configureUnlock = require('@unlock-protocol/unlock-express')
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(cookieParser())
 
-const dateInZone = (zone) => {
+
+const { membersOnly } = configureUnlock(
+  {
+    yieldPaywallConfig: () => {
+      return {
+        locks: {
+          '0xafa8fE6D93174D17D98E7A539A90a2EFBC0c0Fc1': {
+            network: 4,
+          },
+        },
+      }
+    },
+    getUserEthereumAddress: async (request) => {
+      return request.cookies.userAddress
+    },
+    updateUserEthereumAddress: async (
+      request,
+      response,
+      address,
+    ) => {
+      response.cookie('userAddress', address)
+    },
+  },
+  app
+)
+
+const dateInZone = (zone, label) => {
   const d = new Date();
-  const city = (zone.split("/")[1]).replace('_', ' ')
+  const city = label || (zone.split("/")[1]).replace('_', ' ')
   return `${d.toLocaleString('en-US', { timeZone: zone })} in ${city}`
 }
 
@@ -21,9 +47,31 @@ app.get('/', (req, res) => {
   <li>${dateInZone("Asia/Kolkata")}</li>
   <li>${dateInZone("Europe/Paris")}</li>
   <li>${dateInZone("America/New_York")}</li>
-  </ul>`)
+  </ul>
+  <p>Premium version: <a href="/premium">select any time zone</a>!</p>`)
 })
 
+app.get('/premium', membersOnly(), (req, res) => {
+  res.send(`<h1>World Clocks</h1>
+  <p>Thank you for your support!</p>
+  ${zones.map((group) => {
+    return `<div>
+      <h2>${group.group}</h2>
+      <ul>
+        ${group.zones.map(({value, name}) => {
+          return `<li>${dateInZone(value, name)}</li>`
+        }).join("")}
+      </ul>
+    </div>`
+  }).join("")}
+  <p>Premium version: select any time zone!</p>
+  <p><a href="/logout">Logout</a></p>`)
+})
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('userAddress')
+  res.redirect('/')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
